f855da04ac1fbc5067503a52794398f333c0cce0
aparnaapillai0-coder <aparnaapillai0@gmail.com>
2026-06-02 14:48:11 +0530

    Updated Project files

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -25,47 +25,43 @@ class DataIngestion:
     def download_csv_from_gcp_bucket(self):
         try:
             client = storage.Client()
-
+                
             bucket = client.bucket(self.bucket_name)
             blob = bucket.blob(self.file_name)
 
             blob.download_to_filename(RAW_FILE_PATH)
-
             logger.info(f"CSV file is successfully downloaded to {RAW_FILE_PATH}")
-
         except Exception as e:
-            logger.error(f"Error while downloading CSV file: {e}")
-            raise CustomException("Failed to download CSV file", sys)
-
-        
+            logger.error("Error while downloading the CSV file from GCP")
+            raise CustomException("Failed to download CSV file",e)
         
     def data_split_to_train_test(self):
-      try:
-          logger.info("Started with splitting the data into Train & Test Split..")
-          dataset = pd.read_csv(RAW_FILE_PATH)
-          train_data, test_data = train_test_split(dataset, test_size =1 - self.train_test_ratio, random_state = 42)
+        try:
+            logger.info("Started with Splitting the data into Train & Test Split...")
+            dataset = pd.read_csv(RAW_FILE_PATH)
+            train_data, test_data = train_test_split(dataset, test_size=1 - self.train_test_ratio, random_state=42)
 
-          train_data.to_csv(TRAIN_FILE_PATH, index=False)
-          test_data.to_csv(TEST_FILE_PATH, index=False)
+            train_data.to_csv(TRAIN_FILE_PATH)
+            test_data.to_csv(TEST_FILE_PATH)
 
-          logger.info(f"Train data is Saved to {TRAIN_FILE_PATH}")
-          logger.info(f"Test data is Saved to {TEST_FILE_PATH}")
-      
-      except Exception as e:
-           logger.info("Error while splitting the raw data into Train & Test Split")
-           raise CustomException("Failed to split the rawdata into Train and Test Split...!", sys)
+            logger.info(f"Train data is Saved to {TRAIN_FILE_PATH}")
+            logger.info(f"Test data is Saved to {TEST_FILE_PATH}")
 
+        except Exception as e:
+            logger.info("Error while splitting the Raw data into Train & Test Split")
+            raise CustomException("Failed to Split the raw data into Train & Test Split...!",e)
     
     def run(self):
         try:
             logger.info("Data Ingestion is Started...!")
             
-
             # self.download_csv_from_gcp_bucket()
-
+            # logger.info("Data is downloaded Successfully from GCP")
 
             self.data_split_to_train_test()
-            logger.info("Raw data is splitted into Train and Test succeessfully...")
+            logger.info("Raw data is splitted into Train & Test Successfully...")
+            
+            logger.info("Data Ingestion is Successfully Completed...!")
 
         except CustomException as ce:
             logger.error(f"CustomException: {str(ce)}")
@@ -75,6 +71,5 @@ class DataIngestion:
 
 
 if __name__ == "__main__":
-    config = read_yaml(CONFIG_PATH)
-    data_ingestion = DataIngestion(config)
+    data_ingestion = DataIngestion(read_yaml(CONFIG_PATH))
     data_ingestion.run()
