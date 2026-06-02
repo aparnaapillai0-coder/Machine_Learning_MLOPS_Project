1d40eda618a735105ad75a945541131925a7f088
aparnaapillai0-coder <aparnaapillai0@gmail.com>
2026-06-02 14:53:13 +0530

    Updated Project file

--- src/logger.py
+++ src/logger.py
@@ -2,33 +2,23 @@ import logging
 import os
 from datetime import datetime
 
+# Create Logs Directory (only if not exists)
 LOG_DIR = "LOGS"
 os.makedirs(LOG_DIR, exist_ok=True)
 
+# Log file name with timestamp
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILE)
+LOG_FILE_PATH = os.path.join(LOG_DIR,LOG_FILE)
 
+# LOGGING CONFIGURATION
 logging.basicConfig(
     filename=LOG_FILE_PATH,
-    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO
+    format= "[ %(asctime)s ] %(lineno)d - %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
 )
 
+# Create Logger Object 
 def get_logger(name):
     logger = logging.getLogger(name)
-
-    # console output
-    console_handler = logging.StreamHandler()
-    console_handler.setLevel(logging.INFO)
-
-    formatter = logging.Formatter(
-        "[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s"
-    )
-
-    console_handler.setFormatter(formatter)
-
-    if not logger.handlers:
-        logger.addHandler(console_handler)
-
-
+    logger.setLevel(logging.INFO)
     return logger
